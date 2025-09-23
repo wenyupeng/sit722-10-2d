fd96016b3154fa8b608ea597a81b6d415ff54caa
Chris Wen <chriswen430@gmail.com>
2025-09-23 14:45:09 +1000

    update AZURE_CREDENTIALS_STAGING

--- terraform/production/variables.tf
+++ terraform/production/variables.tf
@@ -3,7 +3,7 @@
 variable "prefix" {
   description = "Prefix for all resource names"
   type        = string
-  default     = "chriswen430staging"
+  default     = "chriswen430"
 }
 
 variable "location" {
